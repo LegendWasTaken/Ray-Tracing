69711a3245a5a4105580da948f109bf366d13f5f
LegendWasTaken <caiodcw@gmail.com>
2019-08-16 16:13:30 -0700

    I don't know but github wanted me to add this

--- src/Border.js
+++ src/Border.js
@@ -2,7 +2,7 @@ class Border {
     constructor(x1, y1, x2, y2, t){
         this.s = {x: x1, y: y1};
         this.e = {x: x2, y: y2};
-        this.t = t > 100 || t < 0 || t == null ? 100 : t;
+        this.t = t > 100 || t < 0 || t == null ? 100 : t;   
     }
 
     show(){
